56478a10ad7a9073bf43e17a4362cc1fe5f28b5b
gaoxiang.xiong <gaoxiang.xiong@yeepay.com>
2021-03-22 15:08:46 +0800

    init

---
+++ springboot-mybatis/src/main/java/com/dao/UserEntityMapper.java
@@ -0,0 +1,30 @@
+package com.dao;
+
+import com.entity.UserEntity;
+import com.entity.UserEntityExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserEntityMapper {
+    int countByExample(UserEntityExample example);
+
+    int deleteByExample(UserEntityExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserEntity record);
+
+    int insertSelective(UserEntity record);
+
+    List<UserEntity> selectByExample(UserEntityExample example);
+
+    UserEntity selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") UserEntity record, @Param("example") UserEntityExample example);
+
+    int updateByExample(@Param("record") UserEntity record, @Param("example") UserEntityExample example);
+
+    int updateByPrimaryKeySelective(UserEntity record);
+
+    int updateByPrimaryKey(UserEntity record);
+}
